38dd327fb0eafd22e0bd05a5b571d587ebd8b10c
duff.brian21@gmail.com <duff.brian21@gmail.com>
2017-12-12 13:20:43 -0800

    working weather api

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,4 @@
+
 $(document).ready(function () {
 
     $(".second-page").hide();
@@ -6,23 +7,26 @@ $(document).ready(function () {
 
         $(".main-page").hide();
         $(".second-page").show();
+        // immediately call weather api on submit
+        var userInput = $("#user-input").val().trim();
+        var weatherAPIKey = "166a433c57516f51dfab1f7edaed8413";
+        var queryURL = "https://api.openweathermap.org/data/2.5/weather?" + "zip=" + userInput + "&units=imperial&appid=" + weatherAPIKey;
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).done(function (response) {
+            console.log(response);
+        });
+
     });
 
     $(".back").on("click", function (event) {
         $(".main-page").show();
         $(".second-page").hide();
+
     });
 
-})
+});
 
 // //call api
-// weather api call
-var userInput = $("#user-input").val().trim();
-var weatherAPIKey = "166a433c57516f51dfab1f7edaed8413";
-var queryURL = "https://api.openweathermap.org/data/2.5/weather?" + "zip=" + userInput + "&units=imperial&appid=" + weatherAPIKey;
-$.ajax({
-    url: queryURL,
-    method: "GET"
-}).done(function (response) {
-    console.log(response);
-});
+
