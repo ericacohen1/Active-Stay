3de2fa79b7f2fe4561388b8ead351f274ee8d129
duff.brian21@gmail.com <duff.brian21@gmail.com>
2017-12-12 12:00:16 -0800

    moved weather api call to second page

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,28 +1,28 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-$(".second-page").hide();
+    $(".second-page").hide();
 
-$(".submit").on("click", function(event) {
+    $(".submit").on("click", function (event) {
+
+        $(".main-page").hide();
+        $(".second-page").show();
+    });
+
+    $(".back").on("click", function (event) {
+        $(".main-page").show();
+        $(".second-page").hide();
+    });
+
+})
+
 // //call api
 // weather api call
 var userInput = $("#user-input").val().trim();
-var APIKey = "166a433c57516f51dfab1f7edaed8413";
-var queryURL = "https://api.openweathermap.org/data/2.5/weather?" + "zip=" + userInput + "&units=imperial&appid=" + APIKey;
+var weatherAPIKey = "166a433c57516f51dfab1f7edaed8413";
+var queryURL = "https://api.openweathermap.org/data/2.5/weather?" + "zip=" + userInput + "&units=imperial&appid=" + weatherAPIKey;
 $.ajax({
     url: queryURL,
     method: "GET"
 }).done(function (response) {
     console.log(response);
 });
-
-$(".main-page").hide();
-$(".second-page").show();
-});
-
-$(".back").on("click", function(event) {
-    $(".main-page").show();
-    $(".second-page").hide();
-    });
-
-})
-
