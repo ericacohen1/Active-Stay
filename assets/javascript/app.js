27727bf4f2ee91fd7dafceafc075fcbe2753dc81
duff.brian21@gmail.com <duff.brian21@gmail.com>
2017-12-12 11:28:55 -0800

    weather api working on click event

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,7 +3,18 @@ $(document).ready(function() {
 $(".second-page").hide();
 
 $(".submit").on("click", function(event) {
-//call api
+// //call api
+// weather api call
+var userInput = $("#user-input").val().trim();
+var APIKey = "166a433c57516f51dfab1f7edaed8413";
+var queryURL = "https://api.openweathermap.org/data/2.5/weather?" + "zip=" + userInput + "&units=imperial&appid=" + APIKey;
+$.ajax({
+    url: queryURL,
+    method: "GET"
+}).done(function (response) {
+    console.log(response);
+});
+
 $(".main-page").hide();
 $(".second-page").show();
 });
@@ -14,3 +25,4 @@ $(".back").on("click", function(event) {
     });
 
 })
+
