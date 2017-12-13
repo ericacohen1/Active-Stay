c51994212d6dd6b05b78481a27585f1b75d8563f
Denise Heebner <30487259+dmheebner@users.noreply.github.com>
2017-12-13 13:36:42 -0800

    both modals appearing

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -60,33 +60,33 @@ $(document).ready(function () {
             }
             hotelModal();
 
-            //  function hikingModal() { // Get the modal
-            //      var modal = document.getElementById('myHikingModal');
+           function hikingModal() { // Get the modal
+               var modal = document.getElementById("myHikingModal");
 
-            //      // Get the button that opens the modal
-            //      var btn = document.getElementById("myHikingBtn");
+               // Get the button that opens the modal
+               var btn = document.getElementById("myHikingBtn");
 
-            //      // Get the <span> element that closes the modal
-            //      var span = document.getElementsByClassName("close")[0];
+               // Get the <span> element that closes the modal
+               var span = document.getElementsByClassName("close")[0];
 
-            //      // When the user clicks on the button, open the modal 
-            //      btn.onclick = function () {
-            //          modal.style.display = "block";
-            //      }
+               // When the user clicks on the button, open the modal 
+               btn.onclick = function () {
+                   modal.style.display = "block";
+               }
 
-            //      // When the user clicks on <span> (x), close the modal
-            //      span.onclick = function () {
-            //          modal.style.display = "none";
-            //      }
+               // When the user clicks on <span> (x), close the modal
+               span.onclick = function () {
+                   modal.style.display = "none";
+               }
 
-            //      // When the user clicks anywhere outside of the modal, close it
-            //      window.onclick = function (event) {
-            //          if (event.target == modal) {
-            //              modal.style.display = "none";
-            //          }
-            //      }
-            //  }
-            //  hikingModal();
+               // When the user clicks anywhere outside of the modal, close it
+               window.onclick = function (event) {
+                   if (event.target == modal) {
+                       modal.style.display = "none";
+                   }
+               }
+           }
+           hikingModal();
 
                
            });
