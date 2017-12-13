5a840c0982a81063cf8b50faf1cb2b2b968f4dda
Erica  Cohen <ericacohen@Ericas-MacBook-Air.local>
2017-12-13 10:00:30 -0800

    text field empties

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,17 +1,30 @@
-$(document).ready(function() {
-
-$(".second-page").hide();
-
-$("form").on("submit", function(event) {
-//call api
-event.preventDefault();
-$(".main-page").hide();
-$(".second-page").show();
-});
-
-$(".back").on("click", function(event) {
-    $(".main-page").show();
-    $(".second-page").hide();
+$(document).ready(function () {
+    
+       $(".second-page").hide();
+    
+       $("form").on("submit", function (event) {
+           event.preventDefault();
+    
+           $(".main-page").hide();
+           $(".second-page").show();
+    
+           var userInput = $("#addZip").val();
+           var weatherAPIKey = "166a433c57516f51dfab1f7edaed8413";;
+           var queryURL = "https://api.openweathermap.org/data/2.5/weather?zip=" + userInput + "&units=imperial&appid=" + weatherAPIKey;
+           $.ajax({
+               url: queryURL,
+               method: "GET"
+           }).done(function (response) {
+               console.log(response);
+           });
+    
+    
+       });
+    
+       $(".back").on("click", function (event) {
+           $(".main-page").show();
+           $(".second-page").hide();
+           $("#addZip").val("");
+       });
+    
     });
-
-})
