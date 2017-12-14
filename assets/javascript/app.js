b9667b98675f703c7aaf01235561e77c92bf6968
Denise Heebner <30487259+dmheebner@users.noreply.github.com>
2017-12-14 11:11:46 -0800

    added map

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -11,19 +11,19 @@ function hiking(lat, lon) {
         method: "GET"
     }).done(function (response) {
         console.log(response);
-//Saving off the trails returned from the API to the global trails variable (see above), to be used with details modal.   
+        //Saving off the trails returned from the API to the global trails variable (see above), to be used with details modal.   
         trails = response.trails;
         //loop thru the trails array.    
         for (var i = 0; i < response.trails.length; i++) {
-//Save various data points as variables to display in table.
+            //Save various data points as variables to display in table.
             var hikeName = response.trails[i].name;
             var hikeLocation = response.trails[i].location;
             var hikeLength = response.trails[i].length;
             var hikeSummary = response.trails[i].summary;
             var hikeLink = response.trails[i].url;
 
-//Dynamically creating the content that populates the hiking table.     
-//Saving off the index of the trail we're displaying as data-hiking-index on the details button so we know which button the user clicked.       
+            //Dynamically creating the content that populates the hiking table.     
+            //Saving off the index of the trail we're displaying as data-hiking-index on the details button so we know which button the user clicked.       
             $(".hikingTable > tbody").append("<tr><td><a href='" + hikeLink + "' target='_blank'>" + hikeName + "</a></td><td>" + hikeLocation + "</td><td>" + hikeLength + "</td><td class='hidden-sm hidden-xs'>" + hikeSummary + "</td><td><button type='button' class='btn-xs' data-toggle='modal' data-hiking-index='" + i + "'data-target='#hikingModal'>Details</button></td></tr>")
         }
     })
@@ -31,17 +31,68 @@ function hiking(lat, lon) {
 
 //Created a function that will show certain content in the modal of the details button when it is clicked.
 $("#hikingModal").on("show.bs.modal", function (event) {
-//The event that is getting the button that was clicked.    
+    //The event that is getting the button that was clicked.    
     var button = $(event.relatedTarget);
-//Retrieve hiking index from the button attr.     
+    //Retrieve hiking index from the button attr.     
     var hikingIndexValue = button.data("hiking-index");
-//Using hiking index value to get the correct trail from the global trails array.
+    //Using hiking index value to get the correct trail from the global trails array.
     $("#hike-name").text(trails[hikingIndexValue].name);
     $("#hike-location").text(trails[hikingIndexValue].location);
     $("#hike-summary").text(trails[hikingIndexValue].summary);
     $("#hike-image").attr("src", trails[hikingIndexValue].imgSmallMed);
 
 });
+
+var map;
+var infowindow;
+
+function initMap(lat, lon) {
+
+    var pyrmont = {
+        lat: lat,
+        lng: lon
+    };
+    console.log(lat);
+    console.log(lon);
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: pyrmont,
+        zoom: 15
+
+    });
+
+    infowindow = new google.maps.InfoWindow();
+    var service = new google.maps.places.PlacesService(map);
+    service.nearbySearch({
+        location: pyrmont,
+        radius: 500,
+        type: ['store']
+    }, callback);
+}
+
+function callback(results, status) {
+    if (status === google.maps.places.PlacesServiceStatus.OK) {
+        for (var i = 0; i < results.length; i++) {
+            createMarker(results[i]);
+        }
+    }
+}
+
+function createMarker(place) {
+
+    var placeLoc = place.geometry.location;
+    var marker = new google.maps.Marker({
+        map: {
+            lat: lat,
+            lng: lon
+        },
+        position: place.geometry.location
+    });
+
+    google.maps.event.addListener(marker, 'click', function () {
+        infowindow.setContent(place.name);
+        infowindow.open(map, this);
+    });
+}
 
 
 $(document).ready(function () {
@@ -78,37 +129,9 @@ $(document).ready(function () {
             $(".temp").append("<p>" + "Low: " + response.main.temp_min + "°F" + "</p>");
 
             hiking(response.coord.lat, response.coord.lon);
+            initMap(response.coord.lat, response.coord.lon);
 
         });
-
-
-        // Get the modal
-        var modal = document.getElementById('myModal');
-
-        // Get the button that opens the modal
-        var btn = document.getElementById("myBtn");
-
-        // Get the <span> element that closes the modal
-        var span = document.getElementsByClassName("close")[0];
-
-        // When the user clicks on the button, open the modal 
-        btn.onclick = function () {
-            modal.style.display = "block";
-        }
-
-        // When the user clicks on <span> (x), close the modal
-        span.onclick = function () {
-            modal.style.display = "none";
-        }
-
-        // When the user clicks anywhere outside of the modal, close it
-        window.onclick = function (event) {
-            if (event.target == modal) {
-                modal.style.display = "none";
-            }
-        }
-
-
 
 
     });
